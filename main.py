288ab61a1e9a6db8f377bbc9e39836af7b5801a3
Efekahya <efekahya.ek@gmail.com>
2021-12-10 01:04:57 +0300

    Overall Improvement
    Fixed unnecessary soup parsing
    
    Better debug print !Commented!

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 from auth import auth
 import requests
 import json
-from bs4 import BeautifulSoup as soup
 import os
 import time
 import threading
-
+from bs4 import BeautifulSoup as soup
 # Create a file named auth.py and paste this
 # auth = {'eid': 'username', 'pw': 'pass', 'submit': 'Giriş'}
 # And change 'username' and 'pass' with your credentials
-
 
 
 # Username and password
@@ -72,31 +70,29 @@ with requests.session() as s:
             site_URL = "https://online.deu.edu.tr/direct/announcement/site/" + self.id + ".json"
             response = s.get(site_URL)
 
-            source = soup(response.content, "html.parser")
             # Hatalı dosya çıkabiliyor o yüzden try except bloğu kullandım
             try:
-                result = json.loads(source.text)
+                result = json.loads(response.content)
                 a = result["announcement_collection"]
             except:
                 a = []
             # placeholder olarak kullanıyorum "b" değişkenini nedense direk self.duyuru ya appendlediğimde tüm dersler için değiştiriyor.
             b = []
             for i in range(len(a)):
-                b.append(soup(a[i]["body"], "html.parser").prettify())
+                b.append(soup(a[i]["body"], "html.parser").text)
             self.duyuru = b
 
         def getAssignment(self):
 
             site_URL = "https://online.deu.edu.tr/direct/assignment/site/" + self.id + ".json"
             response = s.get(site_URL)
-            source = soup(response.content, "html.parser")
 
             if response.status_code == 404:
                 pass
             else:
-                result = json.loads(source.text)
-                a = result["assignment_collection"]
 
+                result = json.loads(response.text)
+                a = result["assignment_collection"]
                 if a == []:
                     pass
                 else:
@@ -108,7 +104,7 @@ with requests.session() as s:
                             convertToTuple = time.gmtime(getDueTime)
                             time_string = time.strftime("%d/%m/%Y, %H:%M:%S", convertToTuple)
                             # Ödevin body kısmı
-                            odevContent = soup(a[i]["instructions"], "html.parser").prettify()
+                            odevContent = soup(a[i]["instructions"], "html.parser").text
                             ödevlist.append({"dueTime": time_string, "content": odevContent})
                     self.odev = ödevlist
 
@@ -122,7 +118,7 @@ with requests.session() as s:
             for i in range(len(a)):
                 # Eğer toplantının başlama saati 2+ saati geçmişse id sini alma
                 if (int(time.time()) - int(str(a[i]["startDate"])[:-3])) < 100000:
-                    meetingStartDate = a[i]["startDate"] / 1000 + 9000
+                    meetingStartDate = a[i]["startDate"] / 1000 + 9000+1800
                     convertToTuple = time.gmtime(meetingStartDate)
                     time_string = time.strftime("%d/%m/%Y, %H:%M:%S", convertToTuple)
                     # Aldığın id ler valid mi ?
@@ -136,7 +132,7 @@ with requests.session() as s:
                     else:
                         isMeetingStarted = True
                     meetings.append({"meetingId": a[i]["id"], "meetingStartDate": time_string,
-                                     "siteName": self.name, "available": isMeetingStarted, "meetingUrl": site_URL})
+                                    "siteName": self.name, "available": isMeetingStarted, "meetingUrl": site_URL})
                 self.meeting = meetings
 
 # İlk kez çalıştırılıyorsa
@@ -156,8 +152,6 @@ for i in range(len(dersInfo)):
 
 # Threading
 
-
-
 duyurular = [threading.Thread(target=dersler[i].getAnnouncement, args=()) for i in range(len(dersler))]
 ödevler = [threading.Thread(target=dersler[i].getAssignment, args=()) for i in range(len(dersler))]
 meetingler = [threading.Thread(target=dersler[i].getMeetingIdAndJoin, args=()) for i in range(len(dersler))]
@@ -171,10 +165,19 @@ for i in range(len(dersler)):
     duyurular[i].join()
     ödevler[i].join()
     meetingler[i].join()
+# dersler[5].getAnnouncement()
+# dersler[5].getAssignment()
+# dersler[5].getMeetingIdAndJoin()
 
+# for i in range(len(dersler)):
+#     print(dersler[i].name)
+#     print("MEETING :", dersler[i].meeting)
+    # print("DUYURU :", dersler[i].duyuru)
+    # print("ODEV :", dersler[i].odev)
+# print(dersler[5].odev)
 
-for i in range(len(dersler)):
-    print(dersler[i].meeting)
-    print(dersler[i].name)
-    print(dersler[i].duyuru)
-print()
+# for i in range(len(dersler)):
+#     for j in range(len(dersler[i].odev)):
+#         h = dersler[i].odev[j]
+#         if h != "Yok":
+#             print(h["content"])
