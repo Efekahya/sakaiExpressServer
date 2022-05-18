25528c1f98e810d0d3d5ef7350981a0c4c8b0a3b
Efe Kahyaoğlu <75262924+Efekahya@users.noreply.github.com>
2022-05-18 21:56:11 +0300

    [FRONTEND-BACKEND] Meeting endpoint changed to return all the meetings. Frontend now displays the meeting cards

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
 const request = require("request");
-
+const axios = require("axios");
 exports.register = (req, res) => {
   userPassword = bcrypt.hashSync(req.body.password, 10);
   let user = new User({
@@ -227,7 +227,116 @@ exports.assignmentDetails = async (req, res) => {
     res.status(200).json({ status: "Success", message: assignment });
   });
 };
+// exports.getMeetings = async (req, res) => {
+
+//   const user = await User.findById(req.session.user._id);
+//   if (!user)
+//     return res.status(404).json({ status: "Error", message: "User not found" });
+
+//   const url = "https://online.deu.edu.tr/portal/favorites/list";
+//   const options = {
+//     method: "GET",
+//     url: url,
+//     headers: {
+//       "Content-Type": "application/json",
+//       Cookie: `SAKAI2SESSIONID=${req.session.sakai.token}`,
+//     },
+//   };
+//   let meeting = [];
+//   let meetingList = [];
+//   request(options, (error, response, body) => {
+//     if (error) return res.status(500).json({ status: "Error", message: error });
+//     json = JSON.parse(body);
+//     json = json.favoriteSiteIds;
+//     for (let i = 0; i < json.length; i++) {
+//       const element = json[i];
+//       meeting.push(element);
+//     }
+//     for (let i = 0; i < meeting.length; i++) {
+//       const element = meeting[i];
+//       const url =
+//         "https://online.deu.edu.tr/direct/bbb-tool.json?siteId=" + element;
+
+//       const options = {
+//         method: "GET",
+//         url: url,
+//         headers: {
+//           "Content-Type": "application/json",
+//           Cookie: `SAKAI2SESSIONID=${req.session.sakai.token}`,
+//         },
+//       };
+//       request(options, (error, response, body) => {
+//         if (error)
+//           return res.status(500).json({ status: "Error", message: error });
+//         json = JSON.parse(body);
+//         json = json["bbb-tool_collection"];
+//         let meeting = [];
+//         for (let i = 0; i < json.length; i++) {
+//           const element = json[i];
+//           meeting.push({
+//             id: element.id,
+//             startTime: element.endDate,
+//             joinUrl: element.entityURL + "/joinMeeting",
+//           });
+//         }
+//       });
+//     }
+//   });
+// };
 
 exports.getMeetings = async (req, res) => {
-  res.json({ status: "Success", message: {} });
+  const user = await User.findById(req.session.user._id);
+  if (!user)
+    return res.status(404).json({ status: "Error", message: "User not found" });
+  const url = "https://online.deu.edu.tr/portal/favorites/list";
+  await axios
+    .get(url, {
+      headers: {
+        Cookie: `SAKAI2SESSIONID=${req.session.sakai.token}`,
+      },
+    })
+    .then(async (response) => {
+      json = response.data.favoriteSiteIds;
+      let meeting = [];
+      for (let i = 0; i < json.length; i++) {
+        const element = json[i];
+        meeting.push(element);
+      }
+      let meetingList = [];
+      for (let i = 0; i < meeting.length; i++) {
+        const element = meeting[i];
+        const url =
+          "https://online.deu.edu.tr/direct/bbb-tool.json?siteId=" + element;
+        await axios
+          .get(url, {
+            headers: {
+              Cookie: `SAKAI2SESSIONID=${req.session.sakai.token}`,
+            },
+          })
+          .then((response) => {
+            json = response.data["bbb-tool_collection"];
+            for (let i = 0; i < json.length; i++) {
+              const element = json[i];
+              meetingList.push({
+                name: element.name,
+                ownerDisplayName: element.ownerDisplayName,
+                id: element.id,
+                startTime: element.endDate,
+                joinUrl: element.entityURL + "/joinMeeting",
+              });
+            }
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+
+      meetingList.sort((a, b) => {
+        return b.startTime - a.startTime;
+      });
+      res.status(200).json({ status: "Success", message: meetingList });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 };
--- frontend/src/Components/Cards/MeetingCard.js
+++ frontend/src/Components/Cards/MeetingCard.js
@@ -1,21 +1,27 @@
 import React from "react";
 export default function MeetingCard(item) {
-  let d = new Date(item.item.startTime * 1000).toLocaleString();
+  let d = new Date(item.item.startTime).toLocaleString();
+  let disabled = "primary";
+  let joinButton = "Join Meeting";
   if (item.item.startTime === "") {
     d = "No Meeting Scheduled";
+  }
+  if (Date.now() - item.item.startTime > 0) {
+    disabled = "secondary disabled";
+    joinButton = "Meeting has ended";
   }
 
   return (
     <div className="card mb-2">
       <div className="card-body">
-        <h5 className="card-title">{item.item.title}</h5>
+        <h5 className="card-title">{item.item.name}</h5>
         <h6 className="card-subtitle mb-2 text-muted">{d}</h6>
         <div
           className="card-text"
           dangerouslySetInnerHTML={{ __html: item.item.body }}
         />
-        <a href="/" className="btn btn-primary">
-          Go somewhere
+        <a href={item.item.joinUrl} className={"btn btn-" + disabled}>
+          {joinButton}
         </a>
       </div>
     </div>
